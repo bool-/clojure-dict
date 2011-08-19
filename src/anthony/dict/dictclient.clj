5116b507817d744d918d1948d2bdbdb2aa962e68
Anthony Snavely <anthony.snavely@gmail.com>
2011-08-18 23:28:53 -0700

    cleaned up a bit

--- src/anthony/dict/dictclient.clj
+++ src/anthony/dict/dictclient.clj
@@ -15,7 +15,27 @@
 (defn get-response-data [line]
   (get (str/split line #"\s" 2) 1))
 
-(declare push-command parse-text-block)
+(defn parse-text-block [dict-input]
+  (loop [line (.readLine dict-input)
+         lines []]
+    (if (.startsWith line ".")
+      lines
+      (recur (.readLine dict-input) (conj lines line)))))
+
+(defn push-command [conn-info command & args]
+  (let [dict-sock (:sock conn-info)
+        dict-input (:reader conn-info)
+        dict-output (:writer conn-info)]
+    (write-line dict-output (str/join " " (cons command args)))
+    (let [line (.readLine dict-input)
+          response-code (Integer/parseInt (get-response-code line))
+          text-responses #{114 113 112 111 110 152}
+          join-responses #{114 113 112 152}]
+      (if (text-responses response-code)
+        (if (join-responses response-code)
+          (str/join "\r\n" (parse-text-block dict-input))
+          (parse-text-block dict-input))
+        {:response response-code :reason (get-response-data line)}))))
 
 (defn connect [host port]
   (let [dict-sock (Socket. host port)
@@ -57,29 +77,6 @@
 (defn show-server [conn-info] (push-command conn-info "SHOW SERVER" nil))
 
 (defn status [conn-info] (push-command conn-info "STATUS" nil))
-
-
-(defn parse-text-block [dict-input]
-  (loop [line (.readLine dict-input)
-         lines []]
-    (if (.startsWith line ".")
-      lines
-      (recur (.readLine dict-input) (conj lines line)))))
-
-(defn push-command [conn-info command & args]
-  (let [dict-sock (:sock conn-info)
-        dict-input (:reader conn-info)
-        dict-output (:writer conn-info)]
-    (write-line dict-output (str/join " " (cons command args)))
-    (let [line (.readLine dict-input)
-          response-code (Integer/parseInt (get-response-code line))
-          text-responses [114 113 112 111 110 152]
-          join-responses [114 113 112 152]]
-      (if (some #{response-code} text-responses)
-        (if (some #{response-code} join-responses)
-          (str/join "\r\n" (parse-text-block dict-input))
-          (parse-text-block dict-input))
-        {:response response-code :reason (get-response-data line)}))))
 
 (let [conn-shit (connect "dict.org" 2628)]
   (println (show-server conn-shit)))
